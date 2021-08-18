ce930189491022b5bf3d4a411204ce589889c365
Sunil Jacob <sunil.jacob@ebo.com>
2021-08-18 17:11:15 +0530

    Add subnet and router

--- variables.tf
+++ variables.tf
@@ -22,3 +22,28 @@ variable "region" {
     type       = string
     description = "GCP project region"
 }
+
+variable "subnet_ip_cidr_range" {
+  type        = string
+  description = "Subnet CIDR block"
+}
+
+variable "cluster_secondary_range_name" {
+  type        = string
+  description = "Cluster IP range name for GKE  cluster"
+}
+
+variable "cluster_ipv4_cidr_block" {
+  type        = string
+  description = "Cluster IP ranges for GKE  cluster"
+}
+
+variable "services_secondary_range_name" {
+  type        = string
+  description = "Services IP range name for GKE  cluster"
+}
+
+variable "services_ipv4_cidr_block" {
+  type        = string
+  description = "Services IP ranges for GKE  cluster"
+}
