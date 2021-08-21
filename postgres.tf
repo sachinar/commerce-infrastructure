dd695b3dc5b762e17ae555f7155b211633108c4f
Sunil Jacob <sunil.jacob@ebo.com>
2021-08-21 17:28:51 +0530

    Testing DB

--- postgres.tf
+++ postgres.tf
@@ -20,7 +20,7 @@ resource "random_string" "inventory_app_user_password" {
   override_special = "@#%&*()-_=+[]{}<>:?"
 }
 
-resource "random_string" "dev_team_db_password_orchestrator" {
+resource "random_string" "dev_team_db_passwordinventory" {
   length           = 16
   special          = true
   override_special = "@#%&*()-_=+[]{}<>:?"
@@ -87,7 +87,7 @@ module "inventory_google_postgres" {
   additional_users = [
     {
       name     = "dev-read-user"
-      password = random_string.dev_team_db_password_orchestrator.result
+      password = random_string.dev_team_db_passwordinventory.result
     },
   ]
 
@@ -112,13 +112,13 @@ module "inventory_google_postgres" {
 #   depends_on = [module.gke]
 # }
 
-# resource "google_dns_record_set" "inventory_postgres_a_record" {
-#   # Creating 'A' record for the dns zone
-#   name         = "orchestrator.postgres.${google_dns_managed_zone.private-dns-managed-zone.dns_name}"
-#   project      = var.project_id
-#   managed_zone = google_dns_managed_zone.private-dns-managed-zone.name
-#   type         = "A"
-#   ttl          = 300
-#   rrdatas      = [module.inventory_google_postgres.private_ip_address]
-# }
+resource "google_dns_record_set" "inventory_postgres_a_record" {
+  # Creating 'A' record for the dns zone
+  name         = inventory.postgres.${google_dns_managed_zone.private-dns-managed-zone.dns_name}"
+  project      = var.project_id
+  managed_zone = google_dns_managed_zone.private-dns-managed-zone.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [module.inventory_google_postgres.private_ip_address]
+}
 
