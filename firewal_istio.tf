9324961f8ebd92c7ca88327f515aed675f1dcaa9
Sunil Jacob <sunil.jacob@ebo.com>
2021-08-24 15:30:31 +0530

    <DEVOPS-28> Add firrewall rulee

---
+++ firewal_istio.tf
@@ -0,0 +1,14 @@
+resource "google_compute_firewall" "istio_rule" {
+  name          = "${var.gke_cluster_name}-istio-allow-firewall"
+  project       = var.project_id
+  network       = google_compute_network.network.self_link
+  priority      = 1000
+  direction     = "INGRESS"
+  source_ranges = [var.master_ipv4_cidr_block]
+  target_tags   = ["gke-ebo-dev-gke-cluster-647f4566-node"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["15017"]
+  }
+}
