80f0487d66e9c278d0136e85be0887001b3906c3
Sunil Jacob <sunil.jacob@ebo.com>
2021-09-08 13:41:47 +0530

    <DEVOPS-43> Add docker config

--- jumpbox.tf
+++ jumpbox.tf
@@ -6,11 +6,13 @@ module "jumpbox" {
   source                         = "git::https://github.com/ebomart/terraform-modules.git//jumpbox"
   jumpbox_deployment_name          = var.jumpbox_deployment_name
   jumpbox_namespace                = var.jumpbox_namespace
-#   jumpbox_docker_registry_url      = var.jumpbox_docker_registry_url
-#   jumpbox_docker_registry_user     = var.jumpbox_docker_registry_user
-#   jumpbox_docker_registry_password = var.jumpbox_docker_registry_password
+  jumpbox_docker_registry_url      = "us.gcr.io"
+  jumpbox_docker_registry_user     = "_json_key"
+  jumpbox_docker_registry_password = google_service_account_key.pull_service_account_key.private_key
   jumpbox_number_of_replicas       = var.jumpbox_number_of_replicas
   jumpbox_docker_image             = var.jumpbox_docker_image
   jumpbox_docker_image_tag         = var.jumpbox_docker_image_tag
   jumpbox_docker_image_policy      = var.jumpbox_docker_image_policy
+
+  depends_on = [google_service_account.pull_service_account]
 }
