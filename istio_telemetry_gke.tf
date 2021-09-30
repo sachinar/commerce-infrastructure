8cc083fb76a27390236e7dcdfb0cff969e8e57de
Sunil Jacob <sunil.jacob@ebo.com>
2021-09-30 15:34:55 +0530

    <DEVOPS-74> Fix character issue

--- istio_telemetry_gke.tf
+++ istio_telemetry_gke.tf
@@ -2,23 +2,23 @@
 # ISTIO TELEMETRY PROXY
 # Expose Prometheus Securely
 # ==========================
-resource "kubernetes_secret" "prometheus_proxy_kubernetes_secret" {
-  count    = var.environment == "dev" ? 1:0
-  provider = kubernetes.gke
+# resource "kubernetes_secret" "prometheus_proxy_kubernetes_secret" {
+#   count    = var.environment == "dev" ? 1:0
+#   provider = kubernetes.gke
 
-  metadata {
-    name      = "prometheus-proxy-auth"
-    namespace = "monitoring"
-  }
+#   metadata {
+#     name      = "prometheus-proxy-auth"
+#     namespace = "monitoring"
+#   }
 
-  data = {
-    htpasswd = var.prometheus_htpasswd
-  }
+#   data = {
+#     htpasswd = var.prometheus_htpasswd
+#   }
 
-  depends_on = [
-    module.gke
-  ]
-}
+#   depends_on = [
+#     module.gke
+#   ]
+# }
 
 module "istio_telemetry_proxy" {
   count    = var.environment == "dev" ? 1:0
@@ -26,7 +26,7 @@ module "istio_telemetry_proxy" {
   source       = "git::https://github.com/ebomart/terraform-modules.git//addons/istio-telemetry-proxy"
   release_name = "istio-telemetry-proxy"
   namespace    = "monitoring"
-  values = [file("environment/${var.environment}/istio-telemetry-management-values.yaml")]
+  values = [file("environment/${var.environment}/istio-telemetry-values.yaml")]
   istio_flag             = "false"
   prometheus_secret_name = "prometheus-proxy-auth"
 }
