1017c1eeae2608df391bf2910dfb1f2eae702c66
Sunil Jacob <sunil.jacob@ebo.com>
2021-09-30 16:29:12 +0530

    <DEVOPS-74> Fix provider issue

--- istio_telemetry_gke.tf
+++ istio_telemetry_gke.tf
@@ -21,10 +21,12 @@
 # }
 
 module "istio_telemetry_proxy" {
-  count    = var.environment == "dev" ? 1:0
+
+  count        = var.environment == "dev" ? 1:0
   providers = {
-    kubernetes = kubernetes.gke
-  }  
+    helm = helm.gke
+  }
+
   source       = "git::https://github.com/ebomart/terraform-modules.git//addons/istio-telemetry-proxy"
   release_name = "istio-telemetry-proxy"
   namespace    = "monitoring"
