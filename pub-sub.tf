b8e52b1da702f1cfed636f3cb2b81c70cacd976d
Sunil Jacob <sunil.jacob@ebo.com>
2021-10-20 19:08:09 +0530

    <DEVOPS-70> Fix project id issue

--- pub-sub.tf
+++ pub-sub.tf
@@ -17,7 +17,7 @@ module "pubsub" {
       name                       = "inventory-threshold-push"                                                                               
       push_endpoint              = "${var.push_pubsub_domain}/common/v1/resource"                                         
       # oidc_service_account_email = "sa@example.com"                                          
-      dead_letter_topic          = "projects/my-pubsub-project/topics/invetory-threshold-push-dl-topic"
+      dead_letter_topic          = "projects/${var.project_id}/topics/invetory-threshold-push-dl-topic"
       max_delivery_attempts      = 5                                               
       maximum_backoff            = "600s"                                               
       minimum_backoff            = "300s"                                             
