244fe18e57a4f37be9599256595036f6fc67452a
Sunil Jacob <sunil.jacob@ebo.com>
2021-10-30 19:13:06 +0530

    create resources

--- pub-sub.tf
+++ pub-sub.tf
@@ -1,31 +1,31 @@
-# module "pubsub_push" {
-#     source  = "terraform-google-modules/pubsub/google"
-#   version = "~> 1.8"
+module "pubsub_push" {
+    source  = "terraform-google-modules/pubsub/google"
+  version = "~> 1.8"
 
-#   topic              = "invetory-threshold-push-dl-topic"
-#   project_id         = var.project_id
-#   pull_subscriptions = [
-#     {
-#       name             = "invetory-threshold-push-dl-error"
-#     }
-#   ]
-# }
+  topic              = "invetory-threshold-push-dl-topic"
+  project_id         = var.project_id
+  pull_subscriptions = [
+    {
+      name             = "invetory-threshold-push-dl-error"
+    }
+  ]
+}
 
-# module "pubsub" {
-#   source  = "terraform-google-modules/pubsub/google"
-#   version = "~> 1.8"
+module "pubsub" {
+  source  = "terraform-google-modules/pubsub/google"
+  version = "~> 1.8"
 
-#   topic              = var.topic_name
-#   project_id         = var.project_id
-#   push_subscriptions = [
-#     {
-#       name                       = "inventory-threshold-push"                                                                               
-#       push_endpoint              = "${var.push_pubsub_domain}/common/v1/resource"                                         
-#       # oidc_service_account_email = "sa@example.com"                                          
-#       dead_letter_topic          = "projects/${var.project_id}/topics/invetory-threshold-push-dl-topic"
-#       max_delivery_attempts      = 5                                               
-#       maximum_backoff            = "600s"                                               
-#       minimum_backoff            = "300s"                                             
-#     }
-#   ]
-# }
+  topic              = var.topic_name
+  project_id         = var.project_id
+  push_subscriptions = [
+    {
+      name                       = "inventory-threshold-push"                                                                               
+      push_endpoint              = "${var.push_pubsub_domain}/common/v1/resource"                                         
+      # oidc_service_account_email = "sa@example.com"                                          
+      dead_letter_topic          = "projects/${var.project_id}/topics/invetory-threshold-push-dl-topic"
+      max_delivery_attempts      = 5                                               
+      maximum_backoff            = "600s"                                               
+      minimum_backoff            = "300s"                                             
+    }
+  ]
+}
