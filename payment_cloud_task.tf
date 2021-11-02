78af149c1df7bc3bb7cbaebbfb3bc750a21f1101
Sunil Jacob <sunil.jacob@ebo.com>
2021-11-02 11:11:37 +0530

    <DEVOPS-109> Fix cloud task backoff

--- payment_cloud_task.tf
+++ payment_cloud_task.tf
@@ -7,8 +7,8 @@ resource "google_cloud_tasks_queue" "payment_charge_status_queue" {
   retry_config {
     max_attempts = 5
     max_retry_duration = "864000s"
-    max_backoff = "600s"
-    min_backoff = "3600s"
+    max_backoff = "3600s"
+    min_backoff = "600s"
     max_doublings = 4
   }
 }
